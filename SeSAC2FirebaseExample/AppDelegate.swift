a4d9ffa10d52fa8968f0ddf696285d34c4fb57c9
jaehoon <sjh22058@gmail.com>
2022-10-11 12:07:52 +0900

    update: APNS 통신

--- SeSAC2FirebaseExample/AppDelegate.swift
+++ SeSAC2FirebaseExample/AppDelegate.swift
@@ -8,17 +8,46 @@
 import UIKit
 import Firebase
 import FirebaseCore
-
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        
+        //원격 알림 시스템에 앱을 등록
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        
+        //메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        //현재 등록된 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
         
         return true
     }
@@ -39,4 +68,58 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+// 72 ~ 96 있어도 되고 없어도 되는 부분이지만,
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+    }
+    //포그라운드 알림 수신: 로컬/ 푸시 동일
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+       
+        //.banner, .list: iOS14+
+        completionHandler([.badge, .sound, .banner, .list])
+    }
+    // 유저가 푸시를 클릭했을 때만 수신 확인 가능
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print("사용자가 푸시를 클릭했습니다.")
+        
+        print(response.notification.request.content.body) // 알림 메시지의 내용을 출력할 수 있음
+        
+        print(response.notification.request.content.userInfo)
+        
+        let userInfo = response.notification.request.content.userInfo
+        
+        if userInfo[AnyHashable("sesac")] as? String == "project" {
+            //원하는 화면으로 전환하기
+            print("SESAC PROJECT")
+        } else {
+            print("NOTHING")
+        }
+        
+    }
+    
+    
+}
 
+extension AppDelegate: MessagingDelegate {
+    
+    // 토큰 갱신 모니터링: 토큰 정보가 언제 바뀔까?
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+      // 토큰을 파베에 등록해서 전달!
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
+
+// 기기 -> APNS한테 토큰을 달라고 요청 -> 토큰을 받은 기기는 이 토큰을 서버(FireBase)에 전달해준다.
+// 파베는 여기서 특정 유저(기기)에게 특정 푸쉬를 보낼 수 있게 이미 데이터를 갖고 있음!
